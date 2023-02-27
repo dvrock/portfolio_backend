2f07385790c4565538908557b550cc12b4877650
syedusamatanveer@gmail.com <syedusamatanveer@gmail.com>
2023-02-27 05:11:05 +0500

    first commit

---
+++ Email.js
@@ -0,0 +1,56 @@
+const nodemailer = require("nodemailer");
+const Email = {
+  // The Simple Mail Transfer Protocol is an Internet standard communication protocol for electronic mail transmission. Mail servers and other message transfer agents use SMTP to send and receive mail messages
+  Message: function (req, res) {
+    try {
+      console.log(req.file);
+      let body = JSON.parse(req.body.values);
+      console.log("response:", JSON.parse(req.body.values));
+      var Transport = new nodemailer.createTransport({
+        service: "gmail",
+        host: "smtp.gmail.com",
+        port: 465,
+        secure: false,
+        auth: {
+          user: "syeddkrock@gmail.com",
+          pass: "rnumwheludraocgs",
+        },
+      });
+
+      var mail_options = {
+        from: "syeddkrock@gmail.com",
+        to: "syedusamatanveer@gmail.com",
+        subject: "Details of Client",
+        text: `${body.details}`,
+        html: `<p>name:${body.name} details:${body.details} email:${body.email}</p>`,
+        attachments: [
+          {
+            filename: req.file.filename,
+            path: req.file.path,
+          },
+        ],
+      };
+
+      Transport.sendMail(mail_options, function (err, resp) {
+        if (err) {
+          console.log(err);
+          res.json({
+            status: 500,
+            message: err,
+          });
+        } else {
+          res.json({
+            status: 200,
+            message: resp,
+          });
+          console.log(resp);
+        }
+      });
+    } catch (err) {
+      res.json({
+        message: err.message,
+      });
+    }
+  },
+};
+module.exports = Email;
---
+++ server.js
@@ -0,0 +1,34 @@
+const express = require("express");
+const app = express();
+
+const cors = require("cors");
+var multer = require('multer');
+var upload = multer();
+const bodyparser = require("body-parser");
+app.use(bodyparser.urlencoded({ extended: false }));
+app.use(bodyparser.json());
+// const mongoErrors = require("mongoose-mongodb-errors");
+// mongoose.Promise = global.Promise;
+// mongoose.plugin(mongoErrors);
+let url = 3001||3002||3003;
+const route = require("./routes/routes");
+app.use(express.json());
+app.use(cors())
+
+app.use("/uploads", express.static("./uploads"));
+app.use("/submit", route);
+app.use("*", (req, res,next) => {
+  req.status = 404;
+  const error = new Error("Route not found");
+  res.json({
+    message: error.message,
+    stack: error.stack,
+  }); 
+  next(); 
+});
+
+
+
+app.listen(url, () => {
+  console.log("connected");
+});
