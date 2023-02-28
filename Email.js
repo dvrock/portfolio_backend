ee25d7471296f682a888272f323678a9f634c2d9
syedusamatanveer@gmail.com <syedusamatanveer@gmail.com>
2023-02-28 16:46:52 +0500

    feamanallah

--- Email.js
+++ Email.js
@@ -4,8 +4,7 @@ const Email = {
   // The Simple Mail Transfer Protocol is an Internet standard communication protocol for electronic mail transmission. Mail servers and other message transfer agents use SMTP to send and receive mail messages
   Message: function (req, res) {    
     try {      
-      let body = JSON.parse(req.body.values);
-      console.log("response:", JSON.parse(req.body.values));
+      let body = JSON.parse(req.body.values);      
       var Transport = new nodemailer.createTransport({
         service: "gmail",
         host: "smtp.gmail.com",
