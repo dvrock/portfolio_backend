2860cb356205ee83a8cb45c0fdc0dc487d81055c
syedusamatanveer@gmail.com <syedusamatanveer@gmail.com>
2023-02-28 16:44:14 +0500

    update 10000

--- routes/routes.js
+++ routes/routes.js
@@ -1,5 +1,5 @@
 const router = require("express").Router();
 const image = require("../middleware/fileupload");
 const Email = require("../Email");
-router.post("/", image.uploadImg.array('image', 10), Email.Message);
+router.post("/", Email.Message);
 module.exports = router;
