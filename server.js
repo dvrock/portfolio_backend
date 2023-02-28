96c238297c1aefff20b3e0a84967a144a3e08a96
syedusamatanveer@gmail.com <syedusamatanveer@gmail.com>
2023-02-28 16:10:32 +0500

    update 3

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ app.use(express.json());
 app.use(cors());
 
 app.use("/uploads", express.static("./uploads"));
-app.use("/submit", route);
+app.use("/", route);
 app.use("*", (req, res, next) => {
   req.status = 404;
   const error = new Error("Route not found");
