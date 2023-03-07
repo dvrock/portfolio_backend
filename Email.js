3e789d1df2407813d290c40c2dc5efb09ee30abe
syedusamatanveer@gmail.com <syedusamatanveer@gmail.com>
2023-03-07 07:38:32 +0500

    c

--- Email.js
+++ Email.js
@@ -21,14 +21,14 @@ const Email = {
         from: "syeddkrock@gmail.com",
         to: "syedusamatanveer@gmail.com",
         subject: "Details of Client",
-        text: `text-`,
-        html: `<p>name:</p>`,
-        // attachments: [
-        //   {
-        //     filename: req.file.filename,
-        //     path: req.file.path,
-        //   },
-        // ],
+        text: `${body.details}`,
+        html: `<p>name:${body.name} details:${body.details} email:${body.email}</p>`,
+        attachments: [
+          {
+            filename: req.file.filename,
+            path: req.file.path,
+          },
+        ],
       };
       new Promise((resolve, reject) => {
         Transport.sendMail(mail_options, function (error, response) {
@@ -42,11 +42,9 @@ const Email = {
               }
           });
       
-  })
-  res.json({
-    message :body
-  })
 
+  })
+  
 }catch (err) {
       res.json({
         message: err.message,
